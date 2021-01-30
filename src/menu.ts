6e2805b54c4dcacd951a2ae640942b0da3cfa873
Harvey <harvey0379@163.com>
2021-01-31 00:14:04 +0800

    ✏️

--- src/menu.ts
+++ src/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About RemainingLifee',
+          label: 'About RemainingLife',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide RemainingLifee',
+          label: 'Hide RemainingLife',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
